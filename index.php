7bc32176d53c245cf54586d2810875604701a8ed
Dylan Reuter <djreuter45@gmail.com>
2017-07-31 20:55:38 -0600

    added nav bar and styled a little of the first section

--- index.php
+++ index.php
@@ -37,14 +37,42 @@
 	</head>
 	<body>
 		<header>
+			<nav class="navbar navbar-default">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+								  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Title</a>
+					</div>
 
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Link 1</a></li>
+						<li><a href="#">Link 1</a></li>
+						<li><a href="#">Link 2</a></li>
+						<li><a href="#">Link 3</a></li>
+						<li><a href="#">Link 4</a></li>
+					</ul>
+					</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
 		</header>
-		<div class="container">
+		<!-- fix this class -->
+		<div class="container-fluid">
 			<div class="row">
-				<div class="col-md-9 col-md-offset-2">
+				<div class="col-md-12">
 					<section class="hello-world">
 						<h1>Hello World!</h1>
-						<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information</p>
+						<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+							featured content or information</p>
 						<button class="btn-group btn-group-lg" role="group">Learn More</button>
 					</section>
 				</div> <!-- /.column -->
