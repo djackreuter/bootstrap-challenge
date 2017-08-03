2e884a3c68552ceda81a8ddc14985d9840167097
Dylan Reuter <djreuter45@gmail.com>
2017-08-03 08:00:22 -0600

    added images to bottom section

--- index.php
+++ index.php
@@ -79,11 +79,27 @@
 			<div class="row">
 				<div class="col-md-12 ">
 					<section>
-						<img src="images/placeholder.jpg" alt="city picture"/>
+						<img class ="city-pic" src="images/placeholder.jpg" alt="city picture"/>
 						<p>Nunc vel felis at leo tempus ullamcorper id eu sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer neque lacus, tristique non commodo ut, efficitur ut nibh. Curabitur suscipit purus eu ex viverra, ac ornare tortor feugiat. Vivamus nulla magna, euismod sed convallis ac, rutrum a felis. Morbi sit amet sollicitudin orci. Aliquam erat volutpat. Curabitur faucibus nunc eu neque blandit gravida. Praesent et tellus hendrerit, lobortis mi ac, pulvinar nunc. Sed nec mauris luctus, consequat velit sed, gravida tellus. Pellentesque eu mollis augue. Cras vestibulum nunc nulla, non ornare risus viverra at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In pulvinar, justo ac condimentum tempus, nisi libero porta justo, a ornare enim tortor et enim. Duis maximus, nisi non fermentum pulvinar, elit tellus pretium quam, ut varius tortor enim eu ex. Ut velit lacus, lacinia id enim sit amet, efficitur gravida erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus neque at varius ullamcorper. Integer vel magna in est pharetra tincidunt eu et ex. Nam sit amet quam cursus, auctor dolor et, ultrices ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat, libero ut congue ornare, dui velit fringilla metus, vitae blandit ligula mauris ac velit. Sed sed feugiat quam, ut molestie tellus. Vestibulum suscipit convallis sapien.</p>
 					</section>
 				</div>
 			</div>
 		</div> <!-- /.container -->
+		<div class="container bottom">
+			<div class="row">
+				<div class="col-md-3">
+					<img src="images/soccer.jpg" alt="soccer ball"/>
+				</div>
+				<div class="col-md-3">
+						<img class="image-circle" src="images/soccer.jpg" alt="soccer ball"/>
+				</div>
+				<div class="col-md-3">
+						<img src="images/soccer.jpg" alt="soccer ball"/>
+				</div>
+				<div class="col-md-3">
+						<img src="images/soccer.jpg" alt="soccer ball"/>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
