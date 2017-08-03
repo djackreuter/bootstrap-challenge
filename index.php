4d1520722726b140d6f6b91939fb46f4aee72195
Dylan Reuter <djreuter45@gmail.com>
2017-08-03 09:22:00 -0600

    fixed navbar collapse issue

--- index.php
+++ index.php
@@ -52,14 +52,17 @@
 					</div>
 
 					<!-- Collect the nav links, forms, and other content for toggling -->
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Link 1</a></li>
-						<li><a href="#">Link 1</a></li>
-						<li><a href="#">Link 2</a></li>
-						<li><a href="#">Link 3</a></li>
-						<li><a href="#">Link 4</a></li>
-					</ul>
-				</div><!-- /.container -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link 1</a></li>
+							<li><a href="#">Link 1</a></li>
+							<li><a href="#">Link 2</a></li>
+							<li><a href="#">Link 3</a></li>
+							<li><a href="#">Link 4</a></li>
+						</ul>
+					</div><!-- /.container -->
+				</div><!-- /.collapse navbar-collapse -->
 			</nav>
 		</header>
 		<!-- fix this class -->
@@ -79,8 +82,22 @@
 			<div class="row">
 				<div class="col-md-12 ">
 					<section>
-						<img class ="city-pic" src="images/placeholder.jpg" alt="city picture"/>
-						<p>Nunc vel felis at leo tempus ullamcorper id eu sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer neque lacus, tristique non commodo ut, efficitur ut nibh. Curabitur suscipit purus eu ex viverra, ac ornare tortor feugiat. Vivamus nulla magna, euismod sed convallis ac, rutrum a felis. Morbi sit amet sollicitudin orci. Aliquam erat volutpat. Curabitur faucibus nunc eu neque blandit gravida. Praesent et tellus hendrerit, lobortis mi ac, pulvinar nunc. Sed nec mauris luctus, consequat velit sed, gravida tellus. Pellentesque eu mollis augue. Cras vestibulum nunc nulla, non ornare risus viverra at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In pulvinar, justo ac condimentum tempus, nisi libero porta justo, a ornare enim tortor et enim. Duis maximus, nisi non fermentum pulvinar, elit tellus pretium quam, ut varius tortor enim eu ex. Ut velit lacus, lacinia id enim sit amet, efficitur gravida erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus neque at varius ullamcorper. Integer vel magna in est pharetra tincidunt eu et ex. Nam sit amet quam cursus, auctor dolor et, ultrices ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat, libero ut congue ornare, dui velit fringilla metus, vitae blandit ligula mauris ac velit. Sed sed feugiat quam, ut molestie tellus. Vestibulum suscipit convallis sapien.</p>
+						<img class="city-pic" src="images/placeholder.jpg" alt="city picture"/>
+						<p>Nunc vel felis at leo tempus ullamcorper id eu sem. Interdum et malesuada fames ac ante ipsum
+							primis in faucibus. Integer neque lacus, tristique non commodo ut, efficitur ut nibh. Curabitur
+							suscipit purus eu ex viverra, ac ornare tortor feugiat. Vivamus nulla magna, euismod sed convallis
+							ac, rutrum a felis. Morbi sit amet sollicitudin orci. Aliquam erat volutpat. Curabitur faucibus
+							nunc eu neque blandit gravida. Praesent et tellus hendrerit, lobortis mi ac, pulvinar nunc. Sed nec
+							mauris luctus, consequat velit sed, gravida tellus. Pellentesque eu mollis augue. Cras vestibulum
+							nunc nulla, non ornare risus viverra at. Vestibulum ante ipsum primis in faucibus orci luctus et
+							ultrices posuere cubilia Curae; In pulvinar, justo ac condimentum tempus, nisi libero porta justo,
+							a ornare enim tortor et enim. Duis maximus, nisi non fermentum pulvinar, elit tellus pretium quam,
+							ut varius tortor enim eu ex. Ut velit lacus, lacinia id enim sit amet, efficitur gravida erat.
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus neque at varius ullamcorper.
+							Integer vel magna in est pharetra tincidunt eu et ex. Nam sit amet quam cursus, auctor dolor et,
+							ultrices ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat, libero ut
+							congue ornare, dui velit fringilla metus, vitae blandit ligula mauris ac velit. Sed sed feugiat
+							quam, ut molestie tellus. Vestibulum suscipit convallis sapien.</p>
 					</section>
 				</div>
 			</div>
@@ -90,21 +107,26 @@
 				<div class="col-md-3">
 					<img src="images/soccer.jpg" alt="soccer ball"/>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis, GOAL a fermentum congue,
-						justo enim placerat ipsum, sed malesuada libero nibh non nibh. Mauris tellus leo, egestas quis facilisis rhoncus, rutrum at enim.</p>
+						justo enim placerat ipsum, sed malesuada libero nibh non nibh. Mauris tellus leo, egestas quis
+						facilisis rhoncus, rutrum at enim.</p>
 				</div>
 				<div class="col-md-3">
-						<img class="image-circle" src="images/soccer.jpg" alt="soccer ball"/>
+					<img class="image-circle" src="images/soccer.jpg" alt="soccer ball"/>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis, GOAL a fermentum congue, justo
-						enim placerat ipsum, sed malesuada libero nibh non nibh. Mauris tellus leo, egestas quis facilisis rhoncus, rutrum at enim.</p>
+						enim placerat ipsum, sed malesuada libero nibh non nibh. Mauris tellus leo, egestas quis facilisis
+						rhoncus, rutrum at enim.</p>
 				</div>
 				<div class="col-md-3">
-						<img src="images/soccer.jpg" alt="soccer ball"/>
+					<img src="images/soccer.jpg" alt="soccer ball"/>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis, GOAL a fermentum congue, justo
-						enim placerat ipsum, sed malesuada libero nibh non nibh. Mauris tellus leo, egestas quis facilisis rhoncus, rutrum at enim.</p>
+						enim placerat ipsum, sed malesuada libero nibh non nibh. Mauris tellus leo, egestas quis facilisis
+						rhoncus, rutrum at enim.</p>
 				</div>
 				<div class="col-md-3">
-						<img src="images/soccer.jpg" alt="soccer ball"/>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis, GOAL a fermentum congue, justo enim placerat ipsum, sed malesuada libero nibh non nibh. Mauris tellus leo, egestas quis facilisis rhoncus, rutrum at enim.</p>
+					<img src="images/soccer.jpg" alt="soccer ball"/>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis, GOAL a fermentum congue, justo
+						enim placerat ipsum, sed malesuada libero nibh non nibh. Mauris tellus leo, egestas quis facilisis
+						rhoncus, rutrum at enim.</p>
 				</div>
 			</div>
 		</div>
